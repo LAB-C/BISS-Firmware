869a472c7d42f3d0287975d7d82b2743586851c6
gtg7784 <gtg7784@lab-c.me>
2018-11-17 00:28:08 +0900

    classnames node-sass sass-loader 깔고 대충 버튼 만듦

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.scss';
+import * as classNames from "classnames/bind";
+const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className={cx("App")}>
+            <form>
+
+                <button>찾아보기</button>
+                <input type="file" accept=".hwp, .pdf, .docx"/>
+            </form>
+        </div>
     );
   }
 }
