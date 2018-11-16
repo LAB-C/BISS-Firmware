c056919a9bf59b858c6d4264e48250604c0e70f1
gtg7784 <gtg7784@lab-c.me>
2018-11-17 02:15:55 +0900

    준호가 디바이스 선택도 넣으라해서 넣음

--- src/App.js
+++ src/App.js
@@ -17,11 +17,24 @@ class App extends Component {
     render() {
     return (
         <div className={cx("App")}>
+
+            <div>
+                <div><h1>디바이스 선택</h1></div>
+                <p>
+                    <input type="checkbox"/>
+                    <label>Arduino UNO R3</label> <br/>
+                    <input type="checkbox"/>
+                    <label>Arduino UNO R3</label> <br/>
+                    <input type="checkbox"/>
+                    <label>Arduino UNO R3</label>
+                </p>
+            </div>
+
             <form>
+                <div><h1>펌웨어 선택</h1></div>
                 <button>펌웨어 선택</button>
                 <input type="file" accept=".ino, .txt" onChange={(e) => this.handleChange(e.target.files)}/>
-
-                <label></label>
+                <label>{}</label>
 
                 <div>
                     <table>
