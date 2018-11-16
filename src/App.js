acf1a30df1234e8372a2583868cde3dcce8c8e85
gtg7784 <gtg7784@lab-c.me>
2018-11-17 01:25:07 +0900

    대충 만든거 잘 만듬

--- src/App.js
+++ src/App.js
@@ -4,13 +4,42 @@ import * as classNames from "classnames/bind";
 const cx = classNames.bind(styles);
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(selectorFiles) {
+        console.log(selectorFiles);
+
+    }
+
+    render() {
     return (
         <div className={cx("App")}>
             <form>
+                <button>펌웨어 선택</button>
+                <input type="file" accept=".ino, .txt" onChange={(e) => this.handleChange(e.target.files)}/>
 
-                <button>찾아보기</button>
-                <input type="file" accept=".hwp, .pdf, .docx"/>
+                <label></label>
+
+                <div>
+                    <table>
+                        <thead>
+                        <td>일시</td>
+                        <td>내용</td>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>2018. 11. 15</td>
+                            <td>asdf.ino</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <button type="submit">펌웨어 업로드</button>
+                </div>
             </form>
         </div>
     );
